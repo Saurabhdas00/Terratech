725a81f850e99d8f7a2fa65450a00315c2c53d0e
Saurabh <kumarsaurabh5009@gmail.com>
2024-01-19 01:44:14 +0530

    changes alot added images some content and an ai chatbot but still backend doesnot work have to fix that

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Add this script to handle smooth scrolling when clicking on navigation links
 function redirectToSomewhere(){
     window.location.href = "https://app.powerbi.com/groups/me/reports/e79ad090-c24a-45ea-b612-9297c336fda5?pbi_source=desktop"}
-function redirectToSomewhere(){
-        window.location.href = "https://app.powerbi.com/groups/me/reports/e79ad090-c24a-45ea-b612-9297c336fda5?pbi_source=desktop"}
-function redirectToSomewhere(){
-            window.location.href = "https://app.powerbi.com/groups/me/reports/e79ad090-c24a-45ea-b612-9297c336fda5?pbi_source=desktop"}
-function redirectToSomewhere(){
-                window.location.href = "https://app.powerbi.com/groups/me/reports/e79ad090-c24a-45ea-b612-9297c336fda5?pbi_source=desktop"}
+function redirectTofile1(){
+        window.location.href = "https://app.powerbi.com/groups/me/reports/6d1d3d85-3726-4192-a67b-dc096b8264eb?pbi_source=desktop"}
+function redirectTofile2(){
+            window.location.href = "https://app.powerbi.com/groups/me/reports/19273cf0-580d-4cfb-a078-17f2fd0d11c1/ReportSection36366e729b295454cb92?experience=power-bi"}
+function redirecttofile3(){
+                window.location.href = "https://app.powerbi.com/groups/me/reports/f711bbec-32e9-4e3c-a3cc-b32ca9e1db60?pbi_source=desktop"}
 
 document.addEventListener('DOMContentLoaded', function () {
     var swiper = new Swiper('.swiper-container', {
@@ -69,3 +69,4 @@ document.addEventListener('DOMContentLoaded', function () {
         },
     });
 });
+
